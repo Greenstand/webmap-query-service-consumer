693a2efc718428e48f3ac02fbaf69cd637c835ca
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2021-06-03 02:27:39 +0100

    fix: add capture_taken_at column

---
+++ database/migrations/sqls/20210603012109-add-capture-taken-at-column-down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE capture_feature DROP capture_taken_at;
+ALTER TABLE raw_capture_feature DROP capture_taken_at;
---
+++ database/migrations/sqls/20210603012109-add-capture-taken-at-column-up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE capture_feature ADD capture_taken_at timestamptz;
+ALTER TABLE raw_capture_feature ADD capture_taken_at timestamptz;
