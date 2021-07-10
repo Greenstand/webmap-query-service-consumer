1b516078f6becc4720947de8553e7551709581b6
deanchen <antlet2004@gmail.com>
2021-07-10 10:11:10 +0800

    chore: corectlly insert all region for capture

--- __tests__/integration/rawCaptureCreated.spec.js
+++ __tests__/integration/rawCaptureCreated.spec.js
@@ -14,6 +14,7 @@ describe.only("rawCaptureFeature", () => {
     registerEventHandlers();
     await knex("capture_feature").del();
     await knex("raw_capture_feature").del();
+    await knex("region_assignment").del();
   });
 
   afterEach(async () => {
@@ -28,13 +29,47 @@ describe.only("rawCaptureFeature", () => {
     let result = await knex("raw_capture_feature").select().where("id", capture_in_kenya.id);
     expect(result).lengthOf(1);
     
-    //check the region data, be sure the sample data has been imported from mock/xxx.copy
+    //check the region data, make sure the sample data has been imported from mock/xxx.copy
+    /*
+     * the result from dev DB
+     *
+        treetracker_dev=> select DISTINCT ON (tree_id, zoom_level) trees.id as tree_id, zoom_level, region.id from (
+        select id,lat,lon,estimated_geometric_location from trees where id = 928260
+        ) trees
+        join region
+        ON ST_Contains( region.geom, trees.estimated_geometric_location)
+        join region_zoom on region_zoom.region_id = region.id;
+         tree_id | zoom_level |   id
+        ---------+------------+---------
+          928260 |          1 | 6632476
+          928260 |          2 |   22905
+          928260 |          3 | 6632611
+          928260 |          4 | 6632476
+          928260 |          5 | 6632476
+          928260 |          6 |   22905
+          928260 |          7 |   22905
+          928260 |          8 |   22905
+          928260 |          9 | 2281072
+          928260 |         10 | 2281072
+          928260 |         11 | 2281072
+          928260 |         12 | 2281072
+          928260 |         13 | 2281072
+          928260 |         14 | 2281072
+          928260 |         15 | 5447363
+      (15 rows)
+      */
     result = await knex("region_assignment").select().where({
       map_feature_id: capture_in_kenya.id,
       zoom_level: 9,
       region_id: 2281072,
     });
     expect(result).lengthOf(1);
+
+    result = await knex("region_assignment").select().where({
+      map_feature_id: capture_in_kenya.id,
+    });
+    log.warn("inserted:", result);
+    expect(result).lengthOf(15);
   });
 
 });
