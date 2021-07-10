02c5fa3b298d1dbea79c3acc4f4400f3a692ddf6
deanchen <antlet2004@gmail.com>
2021-07-10 09:59:47 +0800

    chore: can assign capture to region

--- server/infra/database/pg-repositories.js
+++ server/infra/database/pg-repositories.js
@@ -35,25 +35,56 @@ class CaptureFeatureRepository extends BaseRepository {
 
 class RawCaptureFeatureRepository extends BaseRepository {
 
-    constructor(session) {
-        super("raw_capture_feature", session);
-        this._tableName = "raw_capture_feature";
-        this._session = session;
-    }
+  constructor(session) {
+    super("raw_capture_feature", session);
+    this._tableName = "raw_capture_feature";
+    this._session = session;
+  }
 
-    async add(rawCaptureFeature) {
-      log.warn(this._tableName, " add:", rawCaptureFeature);
-        const wellKnownText = `POINT(${rawCaptureFeature.lon} ${rawCaptureFeature.lat})`;
-        const result = await this._session.getDB().raw(`insert into raw_capture_feature (
+  async add(rawCaptureFeature) {
+    log.warn(this._tableName, " add:", rawCaptureFeature);
+    const wellKnownText = `POINT(${rawCaptureFeature.lon} ${rawCaptureFeature.lat})`;
+    const result = await this._session.getDB().raw(`insert into raw_capture_feature (
              id, lat, lon, location, field_user_id, field_username, 
              device_identifier, attributes, created_at, updated_at) 
              values(?, ?, ?, ST_PointFromText(?, 4326), ?, ?, ?, ?, ?, ?)
              returning id`,
-            [rawCaptureFeature.id, rawCaptureFeature.lat, rawCaptureFeature.lon, wellKnownText,
-            rawCaptureFeature.field_user_id, rawCaptureFeature.field_username, rawCaptureFeature.device_identifier,
-            rawCaptureFeature.attributes, rawCaptureFeature.created_at, rawCaptureFeature.created_at]);
-        return result.rows[0]; 
-    }
+      [rawCaptureFeature.id, rawCaptureFeature.lat, rawCaptureFeature.lon, wellKnownText,
+        rawCaptureFeature.field_user_id, rawCaptureFeature.field_username, rawCaptureFeature.device_identifier,
+        rawCaptureFeature.attributes, rawCaptureFeature.created_at, rawCaptureFeature.created_at]);
+    
+    //update region
+    await this.assignRegion(rawCaptureFeature);
+    return result.rows[0]; 
+  }
+    
+  async assignRegion(rawCaptureFeature){
+    log.warn(this._tableName, " assign region:", rawCaptureFeature);
+    const wellKnownText = `POINT(${rawCaptureFeature.lon} ${rawCaptureFeature.lat})`;
+    const result = await this._session.getDB().raw(`
+      INSERT INTO region_assignment
+        (map_feature_id, zoom_level, region_id)
+        SELECT DISTINCT ON (captures.id, zoom_level) captures.id AS map_feature_id, zoom_level, region.id
+        FROM (
+            SELECT *
+            FROM raw_capture_feature
+            WHERE id = ?
+        ) captures
+        JOIN region
+        ON ST_Contains( region.geom, ST_SetSRID(ST_Point(?, ?), 4326))
+        JOIN region_zoom
+        ON region_zoom.region_id = region.id
+        ORDER BY captures.id, zoom_level, region_zoom.priority DESC
+    `,
+      [
+        rawCaptureFeature.id, 
+        rawCaptureFeature.lon, 
+        rawCaptureFeature.lat, 
+      ]
+    );
+    log.warn(this._tableName, "inserted:", result);
+    return true;
+  }
     
 }
 
