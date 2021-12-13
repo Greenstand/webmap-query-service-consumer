041311e590a0057b9502c29238f3d58a29252d94
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-13 15:57:49 -0800

    chore: remove async from global setup

--- .jest/globalSetup.ts
+++ .jest/globalSetup.ts
@@ -1,5 +1,4 @@
-export default async function globalSetup() {
-  // assign if null
+export default function globalSetup() {
   process.env.DATABASE_URL ??= 'postgresql://postgres:passwrd@0.0.0.0/postgres'
   process.env.DATABASE_SCHEMA ??= 'map_features'
   process.env.RABBITMQ_URL ??= 'amqp://guest:guest@localhost'
