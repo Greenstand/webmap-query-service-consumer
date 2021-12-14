814fba9ed8dbc86c69824f915506755783001103
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-14 01:22:00 -0800

    test: create tests for new repo files

---
+++ server/infra/database/RawCaptureFeatureRepository.spec.ts
@@ -0,0 +1,48 @@
+import knex from 'infra/database/knex'
+import { RawCaptureFeature } from 'models/raw-capture-feature'
+
+import RawCaptureFeatureRepository from './RawCaptureFeatureRepository'
+import Session from './session'
+
+const data: RawCaptureFeature = {
+  id: '63e00bca-8eb0-11eb-8dcd-0242ac130003',
+  lat: 0.6383533333333336,
+  lon: 37.663318333333336,
+  field_user_id: 0,
+  field_username: 'fake_name',
+  device_identifier: 'x',
+  attributes: [],
+  created_at: '2021-07-09T03:58:07.814Z',
+  updated_at: '2021-07-09T03:58:07.814Z',
+}
+
+describe('Capture Feature Repo', () => {
+  let repo: RawCaptureFeatureRepository | undefined
+
+  beforeAll(() => {
+    const session = new Session()
+    repo = new RawCaptureFeatureRepository(session)
+  })
+
+  beforeEach(async () => {
+    await knex('raw_capture_feature').del()
+    await knex('region_assignment').del()
+  })
+
+  it('add and assign region', async () => {
+    if (!repo) return
+    await repo.add(data)
+    const addResult = await knex('raw_capture_feature')
+      .select()
+      .where('id', data.id)
+    expect(addResult).toHaveLength(1)
+
+    await repo.assignRegion(data)
+    const assignRegionResult = await knex('region_assignment').select().where({
+      map_feature_id: data.id,
+      zoom_level: 9,
+      region_id: 2281072,
+    })
+    expect(assignRegionResult).toHaveLength(1)
+  })
+})
