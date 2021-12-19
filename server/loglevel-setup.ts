ea700164bc2e2567dbcce91fe84649381f8f40ca
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-19 00:02:33 -0800

    chore: refactor loglevel setup

--- server/loglevel-setup.ts
+++
@@ -1,9 +0,0 @@
-import log from 'loglevel'
-
-export default function initLogLevel() {
-  if (process.env.NODE_LOG_LEVEL) {
-    log.setDefaultLevel(process.env.NODE_LOG_LEVEL as log.LogLevelDesc)
-  } else {
-    log.setDefaultLevel('info')
-  }
-}
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,14 @@
-import { config as dotEnvConfig } from 'dotenv'
-dotEnvConfig()
-// set up log level
-import './setup'
-
+import log from 'loglevel'
 import { initBroker } from 'messaging/broker'
 
 import registerEventHandlers from './services/eventHandlers'
 
 async function main() {
+  if (process.env.NODE_LOG_LEVEL) {
+    log.setDefaultLevel(process.env.NODE_LOG_LEVEL as log.LogLevelDesc)
+  } else {
+    log.setDefaultLevel('info')
+  }
   const broker = await initBroker()
   await registerEventHandlers(broker)
 }
