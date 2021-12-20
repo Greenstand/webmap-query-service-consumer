a2ed326f4f6723ec0cfedb5c3a380e7cee6e1c10
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-20 02:30:09 -0800

    feat: package json sorting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // keep package json sorted
+  'package.json': 'sort-package-json',
+
   // fomat all files recognized by prettier
   '*': 'prettier --ignore-unknown --write',
 
