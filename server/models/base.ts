b53b346368fbd3e13ff568e0ae1959df2e0f93ee
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-08 14:25:03 -0800

    docs: comment batchUpdate function

--- server/models/base.ts
+++ server/models/base.ts
@@ -1,12 +1,13 @@
 import knex, { TableNames } from 'db/knex'
 
+// update objects by ids on tableName with updateObject
 export async function batchUpdate<T>(
   ids: string[],
   updateObject: T,
   tableName: TableNames,
 ) {
-  console.log('batchUpdate: ', updateObject)
+  // remove id from updateObject
   const objectCopy = { ...updateObject } as Omit<T, 'id'>
   const result = await knex(tableName).update(objectCopy).whereIn('id', ids)
-  console.log('result of update:', result)
+  return result
 }
