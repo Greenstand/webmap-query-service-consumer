3e0801b767030792ddc9f8004fb8be6579bbedff
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-08 16:54:53 -0800

    chore: add capture_cluster tablename

--- server/db/knex.ts
+++ server/db/knex.ts
@@ -7,6 +7,7 @@ export enum TableNames {
   CAPTURE_FEATURE = 'capture_feature',
   RAW_CAPTURE_FEATURE = 'raw_capture_feature',
   RAW_CAPTURE_CLUSTER = 'raw_capture_cluster',
+  CAPTURE_CLUSTER = 'capture_cluster',
   REGION_ASSIGNMENT = 'region_assignment',
 }
 
