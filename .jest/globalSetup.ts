b057ed9e6260df9c063ff06ca2b5b967e97d1f14
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-11 20:04:35 -0800

    chore: add stakeholder api env default to test setup

--- .jest/globalSetup.ts
+++ .jest/globalSetup.ts
@@ -4,4 +4,5 @@ export default function globalSetup() {
   process.env.DATABASE_URL ??= 'postgresql://postgres:password@0.0.0.0/postgres'
   process.env.DATABASE_SCHEMA ??= 'map_features'
   process.env.RABBITMQ_URL ??= 'amqp://guest:guest@localhost'
+  process.env.STAKEHOLDER_API_ROUTE ??= 'http://localhost/stakeholder'
 }
