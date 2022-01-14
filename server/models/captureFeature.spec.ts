5ae6f9d7cf21c7b754d1667bd438ee859d484e68
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-14 00:12:29 -0800

    chore: add update capture cluster test

--- server/models/captureFeature.spec.ts
+++ server/models/captureFeature.spec.ts
@@ -2,37 +2,44 @@ import knex, { TableNames } from 'db/knex'
 import {
   addCaptureFeature,
   assignCaptureFeatureRegion,
+  updateCaptureCluster,
 } from 'models/captureFeature'
 import data from '@test/mock/capture_in_kenya.json'
-import { truncateTables } from '@test/utils'
+import {
+  expectClusterHasRegionData,
+  expectTableHasId,
+  prepareRegionData,
+  truncateTables,
+} from '@test/utils'
 
-describe('Creating CaptureFeature', () => {
-  beforeAll(async () => {
-    await truncateTables([
-      TableNames.CAPTURE_FEATURE,
-      TableNames.REGION_ASSIGNMENT,
-    ])
-  })
+beforeAll(async () => {
+  await truncateTables([
+    TableNames.CAPTURE_FEATURE,
+    TableNames.REGION_ASSIGNMENT,
+    TableNames.CAPTURE_CLUSTER,
+  ])
+  await addCaptureFeature(data)
+})
 
-  it('should add the object to the db', async () => {
-    const x = await addCaptureFeature(data)
-    expect(x?.id).toEqual(data.id)
-    console.log(x)
-    const result = await knex(TableNames.CAPTURE_FEATURE)
-      .select()
-      .where('id', data.id)
-    expect(result).toHaveLength(1)
-  })
+it('should add the capture to the db', async () => {
+  await expectTableHasId(TableNames.CAPTURE_FEATURE, data.id)
+})
 
-  it('should assign region', async () => {
-    await assignCaptureFeatureRegion(data)
-    const assignRegionResult = await knex(TableNames.REGION_ASSIGNMENT)
-      .select()
-      .where({
-        map_feature_id: data.id,
-        zoom_level: 9,
-        region_id: 2281072,
-      })
-    expect(assignRegionResult).toHaveLength(1)
-  })
+it('should assign region', async () => {
+  await assignCaptureFeatureRegion(data)
+  const assignRegionResult = await knex(TableNames.REGION_ASSIGNMENT)
+    .select()
+    .where({
+      map_feature_id: data.id,
+      zoom_level: 9,
+      region_id: 2281072,
+    })
+  expect(assignRegionResult).toHaveLength(1)
+})
+
+it('should update cluster', async () => {
+  await prepareRegionData(TableNames.CAPTURE_CLUSTER, data)
+  await updateCaptureCluster(data)
+  const { id } = data
+  await expectClusterHasRegionData(TableNames.CAPTURE_CLUSTER, id)
 })
