bab31e9e7de5c19963849ae5fdbd4f1cc42e0e51
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-14 18:10:09 -0800

    test: create updateRawCaptureCluster test

--- server/models/rawCaptureFeature.spec.ts
+++ server/models/rawCaptureFeature.spec.ts
@@ -1,34 +1,47 @@
 import knex, { TableNames } from 'db/knex'
 import data from '@test/mock/capture_in_kenya.json'
-import { truncateTables } from '@test/utils'
-import { addRawCapture, assignRawCaptureRegion } from './rawCaptureFeature'
+import {
+  expectClusterHasRegionData,
+  expectTableHasId,
+  prepareRegionData,
+  truncateTables,
+} from '@test/utils'
+import {
+  addRawCapture,
+  assignRawCaptureRegion,
+  updateRawCaptureCluster,
+} from './rawCaptureFeature'
 
-const tables = [TableNames.RAW_CAPTURE_FEATURE, TableNames.REGION_ASSIGNMENT]
+const tables = [
+  TableNames.RAW_CAPTURE_FEATURE, //
+  TableNames.REGION_ASSIGNMENT,
+  TableNames.RAW_CAPTURE_CLUSTER,
+]
 
-describe('calling createRawCaptureFeature function', () => {
-  beforeAll(async () => {
-    await truncateTables(tables)
-  })
+beforeAll(async () => {
+  await truncateTables(tables)
+  await addRawCapture(data)
+})
 
-  it('should add the object to the db', async () => {
-    const x = await addRawCapture(data)
-    expect(x?.id).toEqual(data.id)
-    console.log(x)
-    const result = await knex(TableNames.RAW_CAPTURE_FEATURE)
-      .select()
-      .where('id', data.id)
-    expect(result).toHaveLength(1)
-  })
+it('should add the object to the db', async () => {
+  await expectTableHasId(TableNames.RAW_CAPTURE_FEATURE, data.id)
+})
 
-  it('should assign region', async () => {
-    await assignRawCaptureRegion(data)
-    const assignRegionResult = await knex(TableNames.REGION_ASSIGNMENT)
-      .select()
-      .where({
-        map_feature_id: data.id,
-        zoom_level: 9,
-        region_id: 2281072,
-      })
-    expect(assignRegionResult).toHaveLength(1)
-  })
+it('should assign region', async () => {
+  await assignRawCaptureRegion(data)
+  const assignRegionResult = await knex(TableNames.REGION_ASSIGNMENT)
+    .select()
+    .where({
+      map_feature_id: data.id,
+      zoom_level: 9,
+      region_id: 2281072,
+    })
+  expect(assignRegionResult).toHaveLength(1)
+})
+
+it('should update cluster', async () => {
+  await prepareRegionData(TableNames.RAW_CAPTURE_CLUSTER, data)
+  await updateRawCaptureCluster(data)
+  const { id } = data
+  await expectClusterHasRegionData(TableNames.RAW_CAPTURE_CLUSTER, id)
 })
