13a01b1f06bd175eefec5e49e30d1fc280a6eb62
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-18 17:21:06 -0800

    test: onImpactProducer spec uses rabbitmq

--- server/events/onImpactProducerAssigned.spec.ts
+++ server/events/onImpactProducerAssigned.spec.ts
@@ -1,14 +1,16 @@
 import { SetupServerApi } from 'msw/node'
+import waitForExpect from 'wait-for-expect'
 import knex, { TableNames } from 'db/knex'
 import ImpactProducerAssigned from 'interfaces/messages/ImpactProducerAssigned'
 import { SubscriptionNames } from 'messaging/brokerConfig'
+import registerEventHandlers from 'messaging/registerEventHandlers'
 import { addRawCapture } from 'models/rawCaptureFeature'
 import createStakeholderApi from '@test/createStakeholderApi'
 import { expectFeatureToHaveMap } from '@test/featureAssertions'
 import mockCapture from '@test/mock/capture.json'
 import stakeholder from '@test/mock/stakeholder.json'
+import { publishMessage } from '@test/publisher'
 import { truncateTables } from '@test/utils'
-import onImpactProducerAssigned from './onImpactProducerAssigned'
 
 const message: ImpactProducerAssigned = {
   type: 'ImpactProducerAssigned',
@@ -29,6 +31,7 @@ beforeAll(async () => {
 
   mockServer = createStakeholderApi()
   mockServer.listen()
+  await registerEventHandlers()
 })
 
 afterAll(() => {
@@ -38,24 +41,30 @@ afterAll(() => {
 describe(`should handle ${SubscriptionNames.IMPACT_PRODUCER_ASSIGNED} event`, () => {
   it(`should assign impact_producer to capture feature map object`, async () => {
     await knex(TableNames.CAPTURE_FEATURE).insert(mockCapture)
-    await onImpactProducerAssigned(message)
-    await expectFeatureToHaveMap(
-      TableNames.CAPTURE_FEATURE, //
-      mockCapture.id,
-      stakeholder.map,
-    )
+    await publishMessage(SubscriptionNames.IMPACT_PRODUCER_ASSIGNED, message)
+
+    await waitForExpect(async () => {
+      await expectFeatureToHaveMap(
+        TableNames.CAPTURE_FEATURE, //
+        mockCapture.id,
+        stakeholder.map,
+      )
+    })
   })
 
   it('should assign map name to raw captures', async () => {
     await addRawCapture(mockCapture)
-    await onImpactProducerAssigned({
+    await publishMessage(SubscriptionNames.IMPACT_PRODUCER_ASSIGNED, {
       ...message,
       map_feature_kind: 'raw_capture',
     })
-    await expectFeatureToHaveMap(
-      TableNames.RAW_CAPTURE_FEATURE,
-      mockCapture.id,
-      stakeholder.map,
-    )
+
+    await waitForExpect(async () => {
+      await expectFeatureToHaveMap(
+        TableNames.RAW_CAPTURE_FEATURE,
+        mockCapture.id,
+        stakeholder.map,
+      )
+    })
   })
 })
