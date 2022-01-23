e92483f8cf800051f6c7b0bc1c4a8121da39f581
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-22 16:17:05 -0800

    refactor: remove excess try/catch block

--- server/messaging/registerEventHandlers.ts
+++ server/messaging/registerEventHandlers.ts
@@ -6,15 +6,11 @@ import { subscribe } from 'messaging/broker'
 import { SubscriptionNames } from './brokerConfig'
 
 export default async function registerEventHandlers() {
-  try {
-    await subscribe(SubscriptionNames.CAPTURE_CREATED, onCaptureCreated)
-    await subscribe(SubscriptionNames.RAW_CAPTURE_CREATED, onRawCaptureCreated)
-    await subscribe(SubscriptionNames.TOKEN_ASSIGNED, onTokenAssigned)
-    await subscribe(
-      SubscriptionNames.IMPACT_PRODUCER_ASSIGNED,
-      onImpactProducerAssigned,
-    )
-  } catch (e) {
-    console.error('Get error when handling message:', e)
-  }
+  await subscribe(SubscriptionNames.CAPTURE_CREATED, onCaptureCreated)
+  await subscribe(SubscriptionNames.RAW_CAPTURE_CREATED, onRawCaptureCreated)
+  await subscribe(SubscriptionNames.TOKEN_ASSIGNED, onTokenAssigned)
+  await subscribe(
+    SubscriptionNames.IMPACT_PRODUCER_ASSIGNED,
+    onImpactProducerAssigned,
+  )
 }
